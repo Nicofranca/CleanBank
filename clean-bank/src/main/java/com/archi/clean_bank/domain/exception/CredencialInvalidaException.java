0523607087fd034492b82581d344d6808308f96f
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-12 20:21:30 -0300

    feat: adiciona classes exeptions em domain

--- clean-bank/src/main/java/com/archi/clean_bank/domain/exception/CredencialInvalidaException.java
+++ clean-bank/src/main/java/com/archi/clean_bank/domain/exception/CredencialInvalidaException.java
@@ -2,6 +2,6 @@ package com.archi.clean_bank.domain.exception;
 
 public class CredencialInvalidaException extends RuntimeException {
   public CredencialInvalidaException(String message) {
-    super(message);
+    super(message+"s");
   }
 }
