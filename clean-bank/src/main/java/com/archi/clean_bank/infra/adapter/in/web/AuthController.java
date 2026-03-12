78785083ffab1c93905c1ba4fb99a4a87f6a335c
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-12 20:35:32 -0300

    feat: adiciona controller e dto

---
+++ clean-bank/src/main/java/com/archi/clean_bank/infra/adapter/in/web/AuthController.java
@@ -0,0 +1,4 @@
+package com.archi.clean_bank.infra.adapter.in.web;
+
+public class AuthController {
+}
---
+++ clean-bank/src/main/java/com/archi/clean_bank/infra/adapter/in/web/dto/SaqueRequestDTO.java
@@ -0,0 +1,4 @@
+package com.archi.clean_bank.infra.adapter.in.web.dto;
+
+public record SaqueRequestDTO() {
+}
