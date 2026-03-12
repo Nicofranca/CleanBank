922247b50e4459d7a8bdf1753776ba810591a049
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-12 18:29:55 -0300

    feat: inicia projeto

---
+++ clean-bank/src/main/java/com/archi/clean_bank/CleanBankApplication.java
@@ -0,0 +1,13 @@
+package com.archi.clean_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanBankApplication.class, args);
+	}
+
+}
