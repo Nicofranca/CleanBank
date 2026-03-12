6cb7c84a3fce0f6fcbcd4cc96dbb55ba8a47edb1
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-12 20:33:36 -0300

    feat: adiciona classes service

---
+++ clean-bank/src/main/java/com/archi/clean_bank/application/service/LoginService.java
@@ -0,0 +1,6 @@
+package com.archi.clean_bank.application.service;
+
+import com.archi.clean_bank.application.port.in.FazerLoginUseCase;
+
+public class LoginService implements FazerLoginUseCase {
+}
