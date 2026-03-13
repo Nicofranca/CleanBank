1041824ff50673593f4bab8c4a58796bf01a4506
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-12 21:03:01 -0300

    feat: cria classe base client

--- clean-bank/src/main/java/com/archi/clean_bank/domain/model/Client.java
+++ clean-bank/src/main/java/com/archi/clean_bank/domain/model/Client.java
@@ -1,4 +1,31 @@
 package com.archi.clean_bank.domain.model;
 
+import java.util.UUID;
+
 public class Client {
+    private final UUID uuid;
+    private final String cpf;
+    private String nome;
+
+    public Client(String nome, String cpf, UUID uuid) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.uuid = uuid;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
