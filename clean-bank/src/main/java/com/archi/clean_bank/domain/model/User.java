92d6512a15c0cc541cbd42cee0ffa7fd1e778a45
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-04-01 15:15:50 -0300

    feat: criacao da entity pura de User.java

--- clean-bank/src/main/java/com/archi/clean_bank/domain/model/User.java
+++ clean-bank/src/main/java/com/archi/clean_bank/domain/model/User.java
@@ -6,9 +6,12 @@ import java.util.UUID;
 
 public class User {
     private final UUID id;
-    private final String cpf;
+    private final String cpf; //pra login
+
     private String senhaCripto;
-    private String role;
+    private String role; //"ROLE_USER, ROLE_ADMIN"
+
+
 
     public User(UUID id, String cpf, String senhaCripto, String role) {
         this.id = id;
@@ -16,6 +19,8 @@ public class User {
         this.senhaCripto = senhaCripto;
         this.role = role != null ? role: "ROLE_USER";
     }
+
+
 
     public void alterarSenha(String novaSenhaCripto){
         if (novaSenhaCripto == null || novaSenhaCripto.isBlank()){
@@ -25,9 +30,13 @@ public class User {
         if (novaSenhaCripto.equals(this.senhaCripto)){
             throw new RegraNegocioException("A nova senha não pode ser igual a antiga! ");
         }
+
+        this.senhaCripto = novaSenhaCripto;
     }
 
-
+    public void promoverParaAdmin() {
+        this.role = "ROLE_ADMIN";
+    }
 
 
     public UUID getId() {
