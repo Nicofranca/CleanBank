9f11545f7c9a39604c1bdcb18fafa4fd6d195458
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-04-06 21:53:18 -0300

    fix: corrige nomeação de CreadencialInvalidaException

--- clean-bank/src/main/java/com/archi/clean_bank/domain/exception/CredencialInvalidaException.java
+++ clean-bank/src/main/java/com/archi/clean_bank/domain/exception/CredencialInvalidaException.java
@@ -2,6 +2,6 @@ package com.archi.clean_bank.domain.exception;
 
 public class CredencialInvalidaException extends RegraNegocioException {
   public CredencialInvalidaException(String message) {
-    super(message+"s");
+    super(message+"Bruno BOOT");
   }
 }
