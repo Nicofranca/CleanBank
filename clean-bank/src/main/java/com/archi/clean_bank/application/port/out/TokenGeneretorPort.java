917c081ad22d02ba2f8779776900606906298b8b
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-04-30 21:39:14 -0300

    feat: implementação da interface do TokenGeneretorPort

--- clean-bank/src/main/java/com/archi/clean_bank/application/port/out/TokenGeneretorPort.java
+++ clean-bank/src/main/java/com/archi/clean_bank/application/port/out/TokenGeneretorPort.java
@@ -1,4 +1,10 @@
 package com.archi.clean_bank.application.port.out;
 
 public interface TokenGeneretorPort {
+
+    String generate(String subject, String role);
+
+    String getSubjet(String token);
+
+    String getRole(String token);
 }
